097fc1b5960fb59b5d9e92c3bb1473784800ff75
dilpreet <dilpreet@delhiit.com>
2022-08-10 17:20:34 +0530

    average badge made

--- components/MovieBox.jsx
+++ components/MovieBox.jsx
@@ -3,6 +3,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function MovieBox({ data }) {
+  const setVote = (vote) => {
+    if (vote >= 7) {
+      return 'green'
+    } else if (vote >= 4) {
+      return 'blue'
+    } else {
+      return 'red'
+    }
+  }
   return (
     <>
       <div className="movie">
@@ -20,7 +29,7 @@ export default function MovieBox({ data }) {
           <Link href={`/${data.id}`}>
          <a> {data.title}</a></Link>
          </h5>
-        <span>{data.vote_average}</span>
+        <span className={`tag ${setVote(data.vote_average)}`}>{data.vote_average}</span>
 
         </div>
       </div>
